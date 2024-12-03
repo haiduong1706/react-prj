1f74e6accc1f84994f094ccc4777a92a4cfd12de
haiduong1706 <haiiduong1706@gmail.com>
2024-12-03 17:49:23 +0700

    cau dieu kien

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,12 +1,42 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = {
+    isShowList: true,
+  };
+  handleShow = () => {
+    this.setState({
+      isShowList: !this.state.isShowList,
+    });
+  };
   render() {
-    const {name, age} = this.props
+    const { listUsers } = this.props;
+    // console.log(listUsers);
     return (
       <div>
-        <div>My name is {name}</div>
-        <div>My age is {age}</div>
+        <div>
+          <span
+            onClick={() => {
+              this.handleShow();
+            }}
+          >
+            {this.state.isShowList === true ? "Hide List" : "Show List"}
+          </span>
+        </div>
+        {listUsers.map((user, index) => {
+          console.log(user);
+
+          return (
+            <div>
+              {this.state.isShowList && (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <div> My name is {user.name}</div>
+                  <div> My age is {user.age}</div>
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -3,14 +3,19 @@ import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
-
-
+  state = {
+    listUsers: [
+      { id: 1, name: "Duong", age: "18" },
+      { id: 2, name: "Le", age: "23" },
+      { id: 3, name: "Hai", age: "30" },
+    ],
+  };
   //JSX
   render() {
     return (
       <div>
-        <UserInfor/>
-        <DisplayInfor name={"Duong"} age={21} />
+        <UserInfor />
+        <DisplayInfor listUsers={this.state.listUsers}/>
       </div>
     );
   }
