de85ab43c1c288b50fe8c48682f2ccd343b7dcd6
haiduong1706 <haiiduong1706@gmail.com>
2024-12-03 14:49:33 +0700

    state,event

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,12 +1,30 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state ={
+    name: "Duong",
+    age: 21
+  }
+  handleClick(event){
+    console.log("Hello",this.state.name);
+    console.log(event)
+    this.setState({
+      name: "Hai Duong",
+      age: Math.random()
+    })
+  }
+  // hoverClick(event){
+  //   console.log(event)
+  // }
+  
   //JSX
   render() {
     return (
       <div>
-        My first component
-        {Math.random()}
+        My name is {this.state.name}, I am {this.state.age} year old
+        <button onClick={(event)=>{this.handleClick(event)}}>Click me</button>
+        <button onMouseOver={this.hoverClick}>Hover me</button>
+
       </div>
     );
   }
